5f8a9ba1d6ed656b336d1a354c093fa4921ef513
Shubham Thakare <Shubham Thakare@DESKTOP-915JV3T>
2022-09-11 10:37:56 +0530

    [Shubham]: Create two classes demo1 and deno2

---
+++ src/Prg1Ghub/demo1.java
@@ -0,0 +1,8 @@
+package Prg1Ghub;
+
+public class demo1 
+{
+public static void main(String[] args) {
+	System.out.println("Hello");
+}
+}
---
+++ src/Prg1Ghub/demo2.java
@@ -0,0 +1,8 @@
+package Prg1Ghub;
+
+public class demo2 
+{
+public static void main(String[] args) {
+	System.out.println("Hello good mornning");
+}
+}
